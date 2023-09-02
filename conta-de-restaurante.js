b86c8f3e21281d00cb0d6a5d5ce81866ff6fc96d
Gabriela Queiroz Santos de Souza <138119356+gabiqss@users.noreply.github.com>
2023-09-02 14:31:13 -0300

    Update conta-de-restaurante.js

--- conta-de-restaurante.js
+++ conta-de-restaurante.js
@@ -1,13 +1,8 @@
-const restaurantBill = (bill) => {
-    const tax = bill * 0.1;
-    const total = bill + tax;
-    return `$${(total / 5)}`;
+module.exports = function restaurantBill(bill) {
+
+    var tax = bill*0.10;
+
+    var total = (bill+tax)/5;
+
+    return "$"+total;
 };
-
-const output1 = restaurantBill(50);
-const output2 = restaurantBill(100);
-const output3 = restaurantBill(85);
-
-console.log(output1);
-console.log(output2);
-console.log(output3);
